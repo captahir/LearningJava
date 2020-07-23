635a9e4218b895f2ecaa904eb413c1f8c20e0f29
countmerloin <benekli1907drow>
2020-07-23 09:38:44 +0400

    -- wildcards

--- src/main/java/generics/NumGenDemo.java
+++ src/main/java/generics/NumGenDemo.java
@@ -1,6 +1,8 @@
 package generics;
 
 class NumGen<T extends Number> {
+    //here T is bounded with Numbers
+
     T[] array;
 
     public NumGen(T[] array) {
@@ -18,9 +20,17 @@ class NumGen<T extends Number> {
         return sum/array.length;
     }
 }
+
+
 public class NumGenDemo {
     public static void main(String[] args) {
         NumGen<Integer> ints = new NumGen<>(new Integer[] {1, 2, 3, 4, 5});
         System.out.println(ints.findAve(ints.getArray()));
+
+        NumGen<Double> doubles = new NumGen<>(new Double[] {1.5, 2.5, 3.5, 4.5});
+        System.out.println(doubles.findAve(doubles.getArray()));
+
+        //creating String list will fail
+        
     }
 }
